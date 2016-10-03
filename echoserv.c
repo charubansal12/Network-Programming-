95e66947ae607182d837566ec0b6735a807a03f4
frank_anthony <charubansal102@gmail.com>
2016-10-04 00:43:17 +0530

    TCP Synchronous Chat application in C

---
+++ echoserv.c
@@ -0,0 +1,70 @@
+/*Perfectly working code in first commented out section*/
+/*#include<sys/types.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char str[100];
+	int listenfd,connfd;
+	
+	struct sockaddr_in servaddr;
+	listenfd=socket(AF_INET,SOCK_STREAM,0);
+
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family=AF_INET;
+	servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	servaddr.sin_port=htons(22000);
+
+	bind(listenfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+	
+	listen(listenfd,10);
+	connfd=accept(listenfd,(struct sockaddr *)NULL,NULL);
+	
+	while(1)
+	{
+		bzero(str,100);
+		read(connfd,str,100);
+		printf("Echoing back - %s",str);
+		write(connfd,str,strlen(str)+1);
+	}
+}*/
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char sendline[100];
+	char recvline[100];
+	int listenfd,connfd;
+	
+	struct sockaddr_in servaddr;
+	listenfd=socket(AF_INET,SOCK_STREAM,0);
+
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family=AF_INET;
+	servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	servaddr.sin_port=htons(22000);
+
+	bind(listenfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+	
+	listen(listenfd,10);
+	connfd=accept(listenfd,(struct sockaddr *)NULL,NULL);
+	
+	while(1)
+	{
+		bzero(sendline,100);
+		bzero(recvline,100);
+		read(connfd,recvline,100);
+		printf("Client - %s",recvline);
+		
+		fgets(sendline,100,stdin);
+		write(connfd,sendline,strlen(sendline)+1);
+	}
+	return 0;
+}
